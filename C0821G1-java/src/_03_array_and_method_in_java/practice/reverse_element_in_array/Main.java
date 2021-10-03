012fda6f6212a6696e9c8281678c6241e3ceb88a
unknown <letranthaovy1326@gmail.com>
2021-10-03 23:08:48 +0700

    fix exercise

--- C0821G1-java/src/_03_array_and_method_in_java/practice/reverse_element_in_array/Main.java
+++ C0821G1-java/src/_03_array_and_method_in_java/practice/reverse_element_in_array/Main.java
@@ -2,6 +2,6 @@ package _03_array_and_method_in_java.practice.reverse_element_in_array;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println();
     }
 }
