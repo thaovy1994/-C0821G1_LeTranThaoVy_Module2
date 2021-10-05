3b3fcd94f459d9354568d806db8156eb6b6ae083
unknown <letranthaovy1326@gmail.com>
2021-10-06 06:34:03 +0700

    additional

---
+++ C0821G1-java/src/_04_class_and_object_in_java/exercise/QuadraticEquation.java
@@ -0,0 +1,21 @@
+package _04_class_and_object_in_java.exercise;
+
+public class QuadraticEquation {
+    public static void main(String[] args) {
+
+    }
+
+    double a, b, c;
+
+    public QuadraticEquation() {
+    }
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    //getter a, b, c
+    
+}
