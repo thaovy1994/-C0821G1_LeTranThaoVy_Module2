f17934ce2e17c49739c86c9a99a367a5d35dad5d
unknown <letranthaovy1326@gmail.com>
2021-10-15 06:59:22 +0700

    additional

--- C0821G1-java/src/_10_dsa_list/exercise/Linkedlist/MyLinkedList.java
+++ C0821G1-java/src/_10_dsa_list/exercise/Linkedlist/MyLinkedList.java
@@ -1,4 +1,42 @@
 package _10_dsa_list.exercise.Linkedlist;
 
-public class MyLinkedList {
+public class MyLinkedList<E> {
+    private class Node {
+        private Node next;
+        private Object data;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Object getData() {
+            return this.data;
+        }
+    }
+    private Node head;
+    private int numNode = 0;   //mô tả số lượng Node trong list
+
+    //khai báo constructor cho MyLinkedlist
+
+    public MyLinkedList() {
+    }
+
+    //add 1 e vào đầu list
+    public  void addFirst(E element){
+        //khai báo 1 biến temp tham chiếu đến giá trị của head
+        Node temp = head;
+        //biến head nhận giá trị là 1 node mới
+        head = new Node(element);    //node mới có data là e mình truyền vào
+        //chèn e vào e đầu tiên: head.next trỏ đến temp
+        head.next = temp;
+        numNode++;
+    }
+    public void addLast(E element){
+        Node temp = head;
+        while (temp.next !=null){
+            temp=temp.next;
+        }
+        temp.next = new Node(element);
+        numNode++;
+    }
 }
