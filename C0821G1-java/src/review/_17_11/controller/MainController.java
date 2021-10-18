5e46c7957a3c70ad62ff983b9cf47ffad449e983
unknown <letranthaovy1326@gmail.com>
2021-10-18 08:47:58 +0700

    additional

--- C0821G1-java/src/review/_17_11/controller/MainController.java
+++ C0821G1-java/src/review/_17_11/controller/MainController.java
@@ -12,19 +12,19 @@ public class MainController {
         int choices = 0;
         do {
             System.out.println("Manage Student");
-            System.out.println("1.Add New Student");
-            System.out.println("2.Show Student List");
-            System.out.println("3.Edit A Student information");
-            System.out.println("4.Remove A Student");
-            System.out.println("5.Check Id YYY if it is in list ? ");
-            System.out.println("6.Input Id and show his/her information, if Id is not exist, alert ERROR ");
-            System.out.println("7.Arrange Student in point");
+            System.out.println("1.Add new Student");
+            System.out.println("2.Show student list");
+            System.out.println("3.Edit a student information");
+            System.out.println("4.Remove a student from list");
+            System.out.println("5.Check id YYY if it is in list ? ");
+            System.out.println("6.Input id and show his/her information, if id is not exist, alert ERROR ");
+            System.out.println("7.Arrange student in point");
             System.out.println("0.Exit");
             System.out.println("Please choose a number: ");
             choices = Integer.parseInt(input.nextLine());
             switch (choices) {
                 case 1:
-                    System.out.println("1.Add New Student");
+                    System.out.println("1.Add new Student");
                     System.out.println("Input id: ");
                     int id = Integer.parseInt(input.nextLine());
                     System.out.println("Input name: ");
@@ -37,30 +37,30 @@ public class MainController {
                     myStudent.addStudent(student);
                     break;
                 case 2:
-                    System.out.println("2.Show Student List");
+                    System.out.println("2.Show student list");
 //                    myStudent.showStudent();
                     break;
                 case 3:
-                    System.out.println("3.Edit A Student information");
+                    System.out.println("3.Edit a student information");
 
                     break;
                 case 4:
-                    System.out.println("4.Remove A Student");
-                    System.out.println("Input Index to remove");
+                    System.out.println("4.Remove a student");
+                    System.out.println("Input index to remove");
                     int index1 = Integer.parseInt(input.nextLine());
 //                    myStudent.removeStudent();
                     break;
                 case 5:
-                    System.out.println("5.Check Id YYY if it is in list ? ");
+                    System.out.println("5.Check id YYY if it is in list ? ");
                     Student student1 = new Student();
 //                    myStudent.checkId();
                     break;
                 case 6:
-                    System.out.println("6.Input Id and show his/her information, if Id is not exist, alert ERROR ");
+                    System.out.println("6.Input id and show his/her information, if id is not exist, alert ERROR ");
 
                     break;
                 case 7:
-                    System.out.println("7.Arrange Student in point");
+                    System.out.println("7.Arrange student in point");
 //                    myStudent.arrangeInPoint();
                     break;
 
