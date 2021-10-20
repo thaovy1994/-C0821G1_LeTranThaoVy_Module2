5417e36fa8e32dc3d28825dd78ac53707e3b3158
unknown <letranthaovy1326@gmail.com>
2021-10-20 21:38:32 +0700

    additional

--- C0821G1-java/src/_11_stack_queue/exercise/map_count/Main.java
+++ C0821G1-java/src/_11_stack_queue/exercise/map_count/Main.java
@@ -2,16 +2,14 @@ package _11_stack_queue.exercise.map_count;
 
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
         //sử dụng 1 TreeMap để lưu trữ một mục bao gồm một từ và số lượng của nó.
-        int count = 0;
         Map<String, Integer> myTreeMap = new TreeMap<>();
-        myTreeMap.put("apple", 2);
-        myTreeMap.put("durian", 4);
-        myTreeMap.put("banana", 1);
+        myTreeMap.put("aPPle", 5);
 
         //Duyệt toàn bộ từ trong chuỗi.
         //chuyển toàn bộ Key thành chữ hoa hoặc chữ thường.
@@ -20,16 +18,26 @@ public class Main {
             System.out.println("\t" + "Upper Case: " + m.getKey().toUpperCase());
             System.out.println("\t" + "Lower Case: " + m.getKey().toLowerCase());
             System.out.println("-----------------------");
-        }
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input key to check: ");
-        String word = input.nextLine();
-        //Kiểm tra xem trong map có key này hay chưa
 
-        if (myTreeMap.containsKey(word)) {
-            System.out.println("new value: " + myTreeMap.put(word, count++));
-        } else {
-            System.out.println(myTreeMap.put(word, 1));
+            //Kiểm tra xem trong map có key này hay chưa
+            String string = "My World";
+            System.out.println(string);
+            int count = 0;
+
+            Map<Character, Integer> myCounter = new TreeMap<>();
+
+            Scanner input = new Scanner(System.in);
+            System.out.print("Input char to check: ");
+            char character = input.next().charAt(0);
+
+
+            for (int i = 0; i < string.length(); i++) {
+                if (myCounter.containsKey(character)) {
+                    myCounter.put(character, count++);
+                } else {
+                    myCounter.put(character, 1);
+                }
+            }
         }
     }
 }
